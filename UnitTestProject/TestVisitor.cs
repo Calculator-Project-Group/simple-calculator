999c6d008ef3cb0f64f01e9329333e74c4bc6945
luobk <lbk@mail.ustc.edu.cn>
2018-03-21 23:23:51 +0800

    visitor

---
+++ UnitTestProject/TestVisitor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Antlr4.Runtime;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using calculator;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class TestVisitor
+    {
+        [TestMethod]
+        public void TestVisitExp()
+        {
+            string input = @"1 - 2";
+
+            var stream = new AntlrInputStream(input);
+            var lexer = new CalcLexer(stream);
+            var tokens = new CommonTokenStream(lexer);
+            var parser = new CalcParser(tokens);
+            var tree = parser.exp();
+
+            var visitor = new CalcVisitor();
+            var result = visitor.Visit(tree);
+
+
+            //Console.WriteLine(tree.ToStringTree(parser));
+            //Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}

3e807fb8f916be44d0af0faec5b20ba7530e561d
嵩超 王 <songchaow@outlook.com>
2018-03-22 15:32:51 +0800

    add render support for normal exps and fractions, with test passed.

--- UnitTestProject/TestRender.cs
+++ UnitTestProject/TestRender.cs
@@ -19,10 +19,21 @@ namespace UnitTestProject
                 isint = true,
                 result = 50233L
             };
+            Value_node value2 = new Value_node
+            {
+                isint = true,
+                result = 2018L
+            };
             value.Calculate();
             Bracket_node bnode = new Bracket_node
             {
                 internal_node = value
+            };
+            Binary_node bin_node = new Binary_node
+            {
+                node1 = bnode,
+                node2 = value2,
+                op = Binary_node.Operator.ADD
             };
 
             MathRenderer renderer = new MathRenderer();
@@ -34,7 +45,7 @@ namespace UnitTestProject
                 // Initiates the dispatcher thread shutdown when the window closes
                 window.Closed += (s, e) => window.Dispatcher.InvokeShutdown();
 
-                Canvas block = renderer.RenderElement(bnode);
+                Canvas block = renderer.RenderElement(bin_node);
                 window.Content = block;
                 window.Show();
 
@@ -47,6 +58,33 @@ namespace UnitTestProject
             t.Start();
             t.Join();
 
+        }
+        [TestMethod]
+        public void TestFraction()
+        {
+            Value_node value1 = new Value_node
+            {
+                isint = true,
+                result = 50233L
+            };
+            Value_node value2 = new Value_node
+            {
+                isint = true,
+                result = 2018L
+            };
+            Binary_node bin_node = new Binary_node
+            {
+                node1 = value1,
+                node2 = value2,
+                op = Binary_node.Operator.DIVIDE
+            };
+            MathRenderer renderer = new MathRenderer();
+            Window window = new Window();
+            window.Closed += (s, e) => window.Dispatcher.InvokeShutdown();
+            Canvas block = renderer.RenderElement(bin_node);
+            window.Content = block;
+            window.Show();
+            System.Windows.Threading.Dispatcher.Run();
         }
     }
 }
--- calculator/render.cs
+++ calculator/render.cs
@@ -1,57 +1,157 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Shapes;
 
 namespace calculator
 {
-    public class MathText: TextBlock
+    public class MathText : TextBlock
     {
-        public MathText(string formula)
+        public MathText(string formula, double fsize = 18)
         {
             FontFamily = new System.Windows.Media.FontFamily("Cambria Math");
             Text = formula;
-            FontSize = 18;
-            
+            FontSize = fsize;
+
         }
     }
     public class MathRenderer
     {
+        public const double shrinkRatio = (double)5 / 6;
+        public const double defaultFontSize = 18.0;
+        public const double fractionBarHeight = 1.5;
 
         public MathRenderer()
         {
         }
-
-        public Canvas RenderElement(Calc_node exp)
+        /*
+         * Align elements horizontally and return a canvas.
+         * Elements include Canvas and MathText.
+        */
+        public Canvas ArrangeHorizontalGroup(params FrameworkElement[] elements)
         {
             Canvas board = new Canvas();
+            double startWidth = 0, maxHeight=0;
+            // set each element's left margin
+            foreach (var element in elements)
+            {
+                Canvas.SetLeft(element, startWidth);
+                if(element is MathText)
+                {
+                    element.Measure(new Size(Double.PositiveInfinity, Double.PositiveInfinity));
+                    startWidth += element.DesiredSize.Width;
+                    if (maxHeight < element.DesiredSize.Height)
+                        maxHeight = element.DesiredSize.Height;
+                }
+                else
+                {
+                    // it's a canvas.
+                    startWidth += element.Width;
+                    if (maxHeight < element.Height)
+                        maxHeight = element.Height;
+                }
+            }
             
-            if(exp is Value_node)
+            // set each element's top margin
+            foreach(var element in elements)
+            {
+                if(element is MathText)
+                    Canvas.SetTop(element, (maxHeight - element.DesiredSize.Height) / 2);
+                else
+                    Canvas.SetTop(element, (maxHeight - element.Height) / 2);
+                board.Children.Add(element);
+            }
+            board.Width = startWidth;
+            board.Height = maxHeight;
+            return board;
+        }
+        public Canvas ArrangeVerticalGroup(params FrameworkElement[] elements)
+        {
+            Canvas board = new Canvas();
+            double startDepth = 0, maxWidth = 0;
+            // set each element's left margin
+            foreach (var element in elements)
+            {
+                Canvas.SetTop(element, startDepth);
+                if (element is MathText)
+                {
+                    element.Measure(new Size(Double.PositiveInfinity, Double.PositiveInfinity));
+                    startDepth += element.DesiredSize.Height;
+                    if (maxWidth < element.DesiredSize.Width)
+                        maxWidth = element.DesiredSize.Width;
+                }
+                else
+                {
+                    // it's a canvas or sth like that.
+                    startDepth += element.Height;
+                    if (maxWidth < element.Width)
+                        maxWidth = element.Width;
+                }
+            }
+
+            // set each element's top margin
+            foreach (var element in elements)
+            {
+                if (element is MathText)
+                    Canvas.SetLeft(element, (maxWidth - element.DesiredSize.Width) / 2);
+                else
+                    Canvas.SetLeft(element, (maxWidth - element.Width) / 2);
+                board.Children.Add(element);
+            }
+            board.Height = startDepth;
+            board.Width = maxWidth;
+            return board;
+        }
+
+        public Canvas RenderElement(Calc_node exp, double fontsize=defaultFontSize)
+        {
+            Canvas board = null;
+
+            if (exp is Value_node)
             {
                 MathText text = new MathText(exp.result.ToString());
-                text.Measure(new Size(Double.PositiveInfinity, Double.PositiveInfinity));
-                board.Children.Add(text);
-                board.Width = text.DesiredSize.Width;
-                board.Height = text.DesiredSize.Height;
+                board = ArrangeHorizontalGroup(text);
             }
-            else if(exp is Bracket_node)
+            else if (exp is Bracket_node)
             {
                 // we separate brackets and inner text
                 MathText lparen = new MathText("(");
-                Canvas inner_text = RenderElement((exp as Bracket_node).internal_node);
+                Canvas inner_text = RenderElement((exp as Bracket_node).internal_node,fontsize);
                 MathText rparen = new MathText(")");
                 // arrange positions of these elements
-                lparen.Measure(new Size(Double.PositiveInfinity, Double.PositiveInfinity));
-                inner_text.Measure(new Size(Double.PositiveInfinity, Double.PositiveInfinity));
-                rparen.Measure(new Size(Double.PositiveInfinity, Double.PositiveInfinity));
-                
-                Canvas.SetLeft(lparen, 0);
-                board.Children.Add(lparen);
-                Canvas.SetLeft(inner_text, lparen.DesiredSize.Width);
-                board.Children.Add(inner_text);
-                Canvas.SetLeft(rparen, lparen.DesiredSize.Width + inner_text.Width);
-                board.Children.Add(rparen);
-                board.Width = lparen.DesiredSize.Width + rparen.DesiredSize.Width + inner_text.Width;
-                board.Height = inner_text.Height;
+                board = ArrangeHorizontalGroup(lparen, inner_text, rparen);
+            }
+            else if (exp is Binary_node)
+            {
+                Canvas operand1 = null, operand2 = null;
+                switch ((exp as Binary_node).op)
+                {
+                    // horizontal expressions
+                    case Binary_node.Operator.ADD:
+                    case Binary_node.Operator.MINUS:
+                    case Binary_node.Operator.MULTI:
+                        operand1 = RenderElement((exp as Binary_node).node1, fontsize);
+                        MathText add = new MathText("+");
+                        operand2 = RenderElement((exp as Binary_node).node2, fontsize);
+                        board = ArrangeHorizontalGroup(operand1, add, operand2);
+                        break;
+                        
+                    // vertical expressions
+                    case Binary_node.Operator.DIVIDE:
+                        operand1 = RenderElement((exp as Binary_node).node1, fontsize*shrinkRatio);
+                        operand2 = RenderElement((exp as Binary_node).node2, fontsize * shrinkRatio);
+                        Rectangle fractline = new Rectangle();
+                        // fractline's width should be the longer of two elements
+                        fractline.Width = (operand1.Width > operand2.Width ? operand1.Width : operand2.Width)*1.1;
+                        fractline.Height = fractionBarHeight;
+                        fractline.Fill = System.Windows.Media.Brushes.Black;
+                        fractline.HorizontalAlignment = HorizontalAlignment.Center;
+                        board = ArrangeVerticalGroup(operand1, fractline, operand2);
+                        break;
+                    
+                    default:
+                        break;
+                }
             }
 
             return board;

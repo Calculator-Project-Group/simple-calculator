1c269fae2d4ad16e7b4e8a2e26309d74593c48dc
HiderG <37181319+HiderG@users.noreply.github.com>
2018-03-24 18:12:13 +0800

    Update calc_node.cs

--- calculator/calc_node.cs
+++ calculator/calc_node.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,59 +14,125 @@ namespace calculator
         public double fresult;
         public abstract void Calculate();
     }
-    public class Value_node:Calc_node
-    {       
+    public class Value_node : Calc_node
+    {
         public override void Calculate()
         {
             calculated = true;
         }
     }
     // 此类用来表示括号。可嵌套。
-    public class Bracket_node:Calc_node
+    public class Bracket_node : Calc_node
     {
         public Calc_node internal_node = null;
         public override void Calculate()
         {
-            internal_node.Calculate();
-            if(internal_node.isint)
+            if (internal_node != null)
             {
-                isint = true;
-                result = internal_node.result;
-            }
-            else
-            {
-                isint = false;
-                fresult = internal_node.fresult;
+                internal_node.Calculate();
+                if (internal_node.isint)
+                {
+                    isint = true;
+                    result = internal_node.result;
+                }
+                else
+                {
+                    isint = false;
+                    fresult = internal_node.fresult;
+                }
+                calculated = true;
             }
         }
     }
+
     // 此类用来表示各种二元运算表达式和函数。可嵌套。
-    public class Binary_node:Calc_node
+    public class Binary_node : Calc_node
     {
         // 指明运算符：加、减、乘、除、乘方
-        public enum Operator { ADD, MINUS, MULTI, DIVIDE,EXPO,LOG};
+        public enum Operator { ADD, MINUS, MULTI, DIVIDE, EXPO, LOG };
         // 包含两个节点的引用
         public Calc_node node1;
         public Calc_node node2;
         public Operator op;
         public override void Calculate()
         {
-            // 
-            throw new NotImplementedException();
+            isint = node1.isint && node2.isint && (op != Operator.EXPO) && (op != Operator.LOG)
+            && ((op != Operator.MULTI) || (node1.result % node2.result == 0));
+            if (isint)
+            {
+                checked
+                {
+                    try
+                    {
+                        switch (op)
+                        {
+                            case Operator.ADD:
+                                result = node1.result + node2.result; break;
+                            case Operator.MINUS:
+                                result = node1.result - node2.result; break;
+                            case Operator.MULTI:
+                                result = node1.result * node2.result; break;
+                            case Operator.DIVIDE:
+                                result = node1.result / node2.result; break;
+                            default: break;
+                        }
+                    }
+                    catch (OverflowException)
+                    {
+                        node1.fresult = node1.result;
+                        node2.fresult = node2.result;
+                        isint = false;
+                    }
+                }
+            }
+            if (!isint)
+            {
+                switch (op)
+                {
+                    case Operator.ADD:
+                        fresult = node1.fresult + node2.fresult; break;
+                    case Operator.MINUS:
+                        fresult = node1.fresult - node2.fresult; break;
+                    case Operator.MULTI:
+                        fresult = node1.fresult * node2.fresult; break;
+                    case Operator.DIVIDE:
+                        fresult = node1.fresult / node2.fresult; break;
+                    case Operator.EXPO:
+                        fresult = Math.Pow(node1.fresult, node2.fresult); break;
+                    case Operator.LOG:
+                        fresult = Math.Log(node2.fresult, node1.fresult); break;
+                    default: break;
+                }
+            }
+            calculated = true;
         }
     }
+
+
     // 表示各一元函数。可嵌套。
-    public class Unary_node:Calc_node
+    public class Unary_node : Calc_node
     {
-        public enum FuncID { Sin,Cos,Tan};
+        public enum FuncID { Sin, Cos, Tan };
         public FuncID func;
         public Calc_node node1;
         public override void Calculate()
         {
-            int a = 5;
-            a++;
-            throw new NotImplementedException();
+            isint = false;
+            double src = node1.isint ? result : fresult;
+            switch (func)
+            {
+                case FuncID.Sin:
+                    fresult = Math.Sin(src);break;
+                case FuncID.Cos:
+                    fresult = Math.Cos(src); break;
+                case FuncID.Tan:
+                    fresult = Math.Tan(src); break;
+                default: break;
+            }
+            calculated = true;
         }
 
     }
 }
+
+
